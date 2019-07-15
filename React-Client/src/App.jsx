5fda573ae9bcf2f676903cf461df6fba4088ea7c
Satoshi Ono <s14228so@sfc.keio.ac.jp>
2019-07-15 23:40:00 +0900

    フロント微調整

--- React-Client/src/App.jsx
+++ React-Client/src/App.jsx
@@ -6,31 +6,34 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      plans: []
     };
-    this.getData = this.getData.bind(this);
+    this.getPlans = this.getPlans.bind(this);
   }
 
-  getData() {
-    axios.get("http://localhost:8080/plans").then(results => {
-      const data = results.data;
-      console.log(data);
-      this.setState({
-        users: [...data]
-      });
+  componentDidMount() {
+    this.getPlans();
+  }
+
+  async getPlans() {
+    const { data } = await axios.get("http://localhost:8080/plans");
+    console.log(data);
+    this.setState({
+      plans: [...data]
+      //展開記法 (スプレッド記法) は、式を複数の引数または複数の要素に展開して、それぞれ関数呼び出しまたは配列リテラルに渡します。
     });
   }
 
   render() {
-    const users = this.state.users.map(user => {
-      return <li key={user.id}> {user.username} </li>;
+    const plans = this.state.plans.map(plan => {
+      return <li key={plan.id}> {plan.title} </li>;
     });
 
     return (
       <Fragment>
         <div>
-          <button onClick={this.getData}> getData </button> <ul> {users} </ul>{" "}
-        </div>{" "}
+          <button onClick={this.getPlans}> getData </button> <ul> {plans} </ul>
+        </div>
       </Fragment>
     );
   }
