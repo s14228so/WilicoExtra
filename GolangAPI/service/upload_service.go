5558a8cc940c91825bf91009e57bf66ebdcd6fe6
Satoshi Ono <s14228so@sfc.keio.ac.jp>
2019-09-03 11:23:05 +0900

    ショキカする

--- GolangAPI/service/upload_service.go
+++ GolangAPI/service/upload_service.go
@@ -8,9 +8,9 @@ import (
 	"path"
 
 	"cloud.google.com/go/storage"
+	"github.com/GoogleCloudPlatform/golang-samples/getting-started/bookshelf"
 	"github.com/gin-gonic/gin"
 	"github.com/gofrs/uuid"
-	"github.com/s14228so/WilicoExtra/GolangAPI/bookshelf"
 )
 
 func configureStorage(bucketID string) (*storage.BucketHandle, error) {
@@ -39,12 +39,15 @@ func (s Service) CreateImageModel(c *gin.Context) (url string, err error) {
 		fmt.Println("なんかerrorだよ")
 		return "", err
 	}
+	StorageBucketName := "wilico-extra"
+	StorageBucket, err := configureStorage(StorageBucketName)
 
-	if bookshelf.StorageBucket == nil {
+	fmt.Printf("bucket: %v", StorageBucket)
+	if StorageBucket == nil {
 		fmt.Println("storageないよ！")
 		return "", errors.New("storage bucket is missing - check config.go")
 	}
-	fmt.Printf("bookshelf: %v", bookshelf.StorageBucketName)
+	fmt.Printf("bookshelf: %v", StorageBucketName)
 
 	var publicURL string
 	var name string
@@ -56,7 +59,7 @@ func (s Service) CreateImageModel(c *gin.Context) (url string, err error) {
 		fmt.Printf("name: %v", name)
 		// go routine などを呼び出す元の方でオブジェクトを生成する。context.Background() は空のコンテキストを生成する。
 		ctx := context.Background()
-		w := bookshelf.StorageBucket.Object(name).NewWriter(ctx)
+		w := StorageBucket.Object(name).NewWriter(ctx)
 		fmt.Printf("w: %v", w)
 		// Warning: storage.AllUsers gives public read access to anyone.
 		w.ACL = []storage.ACLRule{{Entity: storage.AllUsers, Role: storage.RoleReader}}
@@ -80,37 +83,3 @@ func (s Service) CreateImageModel(c *gin.Context) (url string, err error) {
 	return fmt.Sprintf(publicURL, bookshelf.StorageBucketName, name), nil
 
 }
-
-// func (s Service) CreateImageodel(c *gin.Context) (Image, error) {
-// 	var u Image
-
-// 	form, err := c.MultipartForm()
-// 	post := c.PostForm("key")
-// 	if err != nil {
-// 		c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
-// 		return u, err
-// 	}
-
-// 	files := form.File[post]
-// 	log.Printf("file: %v", files) //ここが空
-
-// 	for _, file := range files {
-// 		dstDir := "./public/unko.jpg"
-// 		if err := c.SaveUploadedFile(file, dstDir); err != nil {
-// 			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
-// 			return u, err
-// 		}
-// 	}
-
-// 	c.String(http.StatusOK, fmt.Sprintf("Uploaded successfully %d files.", len(files)))
-
-// 	// if err := c.BindJSON(&u); err != nil {
-// 	// 	return u, err
-// 	// }
-
-// 	// if err := db.Create(&u).Error; err != nil {
-// 	// 	return u, err
-// 	// }
-
-// 	return u, nil
-// }
